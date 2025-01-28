a887aeec74d94740fd2fb2a05f38ae6eb5dd204c
Sunil Singh <er.techworld@gmail.com>
2025-01-28 12:35:38 +0530

    created project structure

---
+++ DTO/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DTO
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public int RollNumber { get; set; }
+        public decimal Fee { get; set; }
+    }
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- StudentManagement/Controllers/WeatherForecastController.cs
+++ StudentManagement/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using DTO;
 
 namespace StudentManagement.Controllers
 {
@@ -21,6 +22,7 @@ namespace StudentManagement.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +31,74 @@ namespace StudentManagement.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            try
+            {
+                List<User> students = new List<User>();
+                User student = new User()
+                {
+                    FullName = "Anil Kumar",
+                    RollNumber = 100,
+                    Fee = 10.50M
+                };
+                students.Add(student);
+
+                student = new User()
+                {
+                    FullName = "Payal",
+                    RollNumber = 101,
+                    Fee = 100.50M
+                };
+                students.Add(student);
+                return Ok(students);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Input string is wrong, please validate your program");
+            }
+            finally
+            {
+
+            }
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public IActionResult AddStudent(User model)
+        {
+            try
+            {
+
+                return Ok("Student has been added successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("unable to add student due to these reasons : " + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginModel model)
+        {
+            try
+            {
+                User user = new User()
+                {
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                return Ok("Student has been loggedin successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("unable to login student due to these reasons : " + ex.Message);
+            }
+        }
     }
+
 }
